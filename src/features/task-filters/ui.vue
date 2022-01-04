2c89f80f86be85152e5887fc31213d8d48a92a23
magistrfox <magistrfox.business@gmail.com>
2022-01-04 13:20:40 +0300

    fix filters group bug

--- src/features/task-filters/ui.vue
+++ src/features/task-filters/ui.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
   import {RadioButton, RadioGroup} from 'ant-design-vue';
-  import {computed} from 'vue';
+  import {computed, ref} from 'vue';
   import {useStore} from 'vuex';
   import {taskModel} from 'entities/task';
   import {filtersList, getFilterById, DEFAULT_FILTER} from './config'
 
   const store = useStore();
 
+  const filtersGroupModel = ref<number>(DEFAULT_FILTER);
+
   const isListLoading = computed(() => store.state[taskModel.NAMESPACE].isListLoading);
-  const onFilterClick = (id: number) => store.commit(
-    taskModel.mutations.setQueryConfig, getFilterById(id).config
-  )
+  const onFilterClick = (e) => {
+    const id = e.target.value;
+    store.commit(
+      taskModel.mutations.setQueryConfig, getFilterById(id).config
+    )
+  }
 </script>
 
 <template>
-  <RadioGroup :defaultValue="DEFAULT_FILTER"
-              buttonStyle="solid"
+  <RadioGroup buttonStyle="solid"
+              @change="onFilterClick"
+              v-model:value="filtersGroupModel"
   >
     <template v-for="{title, id} in filtersList" :key="id">
       <RadioButton
                 :value="id"
-                @click="onFilterClick(id)"
                 :disabled="isListLoading"
       >
         {{title}}
